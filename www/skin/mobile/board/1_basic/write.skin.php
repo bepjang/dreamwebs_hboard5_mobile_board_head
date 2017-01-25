f419adad5bbeb802aa1b79875b8a85fee6d45e0a
JUNGWON WOO <bepjang@gmail.com>
2017-01-25 11:11:06 +0900

    init 모바일 글쓰기 상단 내용 감춤

---
+++ www/skin/mobile/board/1_basic/write.skin.php
@@ -0,0 +1,247 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+// add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
+add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 0);
+?>
+
+<section id="bo_w">
+    <h2 id="container_title"><?php echo $g5['title'] ?></h2>
+
+    <form name="fwrite" id="fwrite" action="<?php echo $action_url ?>" onsubmit="return fwrite_submit(this);" method="post" enctype="multipart/form-data" autocomplete="off">
+    <input type="hidden" name="w" value="<?php echo $w ?>">
+    <input type="hidden" name="bo_table" value="<?php echo $bo_table ?>">
+    <input type="hidden" name="wr_id" value="<?php echo $wr_id ?>">
+    <input type="hidden" name="sca" value="<?php echo $sca ?>">
+    <input type="hidden" name="sfl" value="<?php echo $sfl ?>">
+    <input type="hidden" name="stx" value="<?php echo $stx ?>">
+    <input type="hidden" name="spt" value="<?php echo $spt ?>">
+    <input type="hidden" name="sst" value="<?php echo $sst ?>">
+    <input type="hidden" name="sod" value="<?php echo $sod ?>">
+    <input type="hidden" name="page" value="<?php echo $page ?>">
+    <?php
+    $option = '';
+    $option_hidden = '';
+    if ($is_notice || $is_html || $is_secret || $is_mail) {
+        $option = '';
+        if ($is_notice) {
+            $option .= PHP_EOL.'<input type="checkbox" id="notice" name="notice" value="1" '.$notice_checked.'>'.PHP_EOL.'<label for="notice">공지</label>';
+        }
+
+        if ($is_html) {
+            if ($is_dhtml_editor) {
+                $option_hidden .= '<input type="hidden" value="html1" name="html">';
+            } else {
+                $option .= PHP_EOL.'<input type="checkbox" id="html" name="html" onclick="html_auto_br(this);" value="'.$html_value.'" '.$html_checked.'>'.PHP_EOL.'<label for="html">html</label>';
+            }
+        }
+
+        if ($is_secret) {
+            if ($is_admin || $is_secret==1) {
+                $option .= PHP_EOL.'<input type="checkbox" id="secret" name="secret" value="secret" '.$secret_checked.'>'.PHP_EOL.'<label for="secret">비밀글</label>';
+            } else {
+                $option_hidden .= '<input type="hidden" name="secret" value="secret">';
+            }
+        }
+
+        if ($is_mail) {
+            $option .= PHP_EOL.'<input type="checkbox" id="mail" name="mail" value="mail" '.$recv_email_checked.'>'.PHP_EOL.'<label for="mail">답변메일받기</label>';
+        }
+    }
+
+    echo $option_hidden;
+    ?>
+    <div id="bo_w" class="tbl_frm01 tbl_wrap">
+        <table>
+        <tbody>
+        <?php if ($is_name) { ?>
+        <tr>
+            <td><input type="text" name="wr_name" value="<?php echo $name ?>" id="wr_name" required class="frm_input required" maxlength="20" placeholder="이름"></td>
+        </tr>
+        <?php } ?>
+
+        <?php if ($is_password) { ?>
+        <tr>
+            <td><input type="password" name="wr_password" id="wr_password" <?php echo $password_required ?> class="frm_input <?php echo $password_required ?>" maxlength="20" placeholder="비밀번호"></td>
+        </tr>
+        <?php } ?>
+
+        <?php if ($is_email) { ?>
+        <tr>
+            <td><input type="email" name="wr_email" value="<?php echo $email ?>" id="wr_email" class="frm_input email" maxlength="100" placeholder="이메일"></td>
+        </tr>
+        <?php } ?>
+
+
+
+<?php /*?>        <?php if ($option) { ?>
+        <tr>
+            <th scope="row">옵션</th>
+            <td><?php echo $option ?></td>
+        </tr>
+        <?php } ?><?php */?>
+
+        <?php if ($is_category) { ?>
+        <tr>
+            <td>
+                <select class="required" id="ca_name" name="ca_name" required>
+                    <option value="">선택하세요</option>
+                    <?php echo $category_option ?>
+                </select>
+            </td>
+        </tr>
+        <?php } ?>
+
+        <tr>
+            <td><input type="text" style="height:40px;" name="wr_subject" value="<?php echo $subject ?>" id="wr_subject" required class="frm_input required" placeholder="제목"></td>
+        </tr>
+
+        <tr>
+            <td class="wr_content">
+		<?php if($editor_form) { ?>
+            <?php echo $editor_form; ?>
+		<?php } else { ?>
+			<div class="wr_content_wrap" style="margin-right:4px;">
+				<textarea style="height:100px;" id="wr_content" name="wr_content" rows="10" itemname="내용" required placeholder="내용"
+		        <?php if ($write_min || $write_max) { ?>onkeyup="check_byte('wr_content', 'char_count');"<?php } ?>><?php echo $content; ?></textarea>
+			    <?php if ($write_min || $write_max) { ?><script> check_byte('wr_content', 'char_count'); </script><?php } ?>
+			</div>
+        <?php } ?>
+            </td>
+        </tr>
+
+<?php /*?>        <?php for ($i=1; $is_link && $i<=G5_LINK_COUNT; $i++) { ?>
+        <tr>
+            <th scope="row"><label for="wr_link<?php echo $i ?>">링크 #<?php echo $i ?></label></th>
+            <td><input type="url" name="wr_link<?php echo $i ?>" value="<?php if($w=="u"){echo$write['wr_link'.$i];} ?>" id="wr_link<?php echo $i ?>" class="frm_input wr_link"></td>
+        </tr>
+        <?php } ?><?php */?>
+
+        <tr>
+            <td>
+            	<p style="color:red;font-size:13px;">파일첨부시 선택창이 안 나올경우  크롬브라우져를 이용해 주세요</p>
+            </td>
+        </tr>
+        
+        <?php for ($i=0; $is_file && $i<$file_count; $i++) { ?>
+        <tr>
+            <td>
+                <input type="file" name="bf_file[]" title="파일첨부 <?php echo $i+1 ?> :  용량 <?php echo $upload_max_filesize ?> 이하만 업로드 가능" class="frm_file frm_input">
+                <?php if ($is_file_content) { ?>
+                <input type="text" name="bf_content[]" value="<?php echo $file[$i]['bf_content']; ?>" title="파일 설명을 입력해주세요." class="frm_file frm_input">
+                <?php } ?>
+                <?php if($w == 'u' && $file[$i]['file']) { ?>
+                <input type="checkbox" id="bf_file_del<?php echo $i ?>" name="bf_file_del[<?php echo $i; ?>]" value="1"> <label for="bf_file_del<?php echo $i ?>"><?php echo $file[$i]['source'].'('.$file[$i]['size'].')'; ?> 파일 삭제</label>
+                <?php } ?>
+            </td>
+        </tr>
+        <?php } ?>
+
+        <?php if ($is_guest) { //자동등록방지 ?>
+        <tr>
+            <th scope="row">자동등록방지</th>
+            <td>
+                <?php echo $captcha_html ?>
+            </td>
+        </tr>
+        <?php } ?>
+
+        </tbody>
+        </table>
+    </div>
+
+    <div class="btn_confirm">
+        <input type="submit"  style="width:97%; height:38px; font-size:15px; border-color: #CCC;" value="등록하기" id="btn_submit" class="btn_b02" accesskey="s">       
+    </div>
+    
+
+    
+    </form>
+</section>
+
+<script>
+<?php if($write_min || $write_max) { ?>
+// 글자수 제한
+var char_min = parseInt(<?php echo $write_min; ?>); // 최소
+var char_max = parseInt(<?php echo $write_max; ?>); // 최대
+check_byte("wr_content", "char_count");
+
+$(function() {
+    $("#wr_content").on("keyup", function() {
+        check_byte("wr_content", "char_count");
+    });
+});
+
+<?php } ?>
+function html_auto_br(obj)
+{
+    if (obj.checked) {
+        result = confirm("자동 줄바꿈을 하시겠습니까?\n\n자동 줄바꿈은 게시물 내용중 줄바뀐 곳을<br>태그로 변환하는 기능입니다.");
+        if (result)
+            obj.value = "html2";
+        else
+            obj.value = "html1";
+    }
+    else
+        obj.value = "";
+}
+
+function fwrite_submit(f)
+{
+    <?php echo get_editor_js('wr_content', $is_dhtml_editor); ?>
+    <?php echo chk_editor_js('wr_content', $is_dhtml_editor); ?>
+
+    var subject = "";
+    var content = "";
+    $.ajax({
+        url: g5_bbs_url+"/ajax.filter.php",
+        type: "POST",
+        data: {
+            "subject": f.wr_subject.value,
+            "content": f.wr_content.value
+        },
+        dataType: "json",
+        async: false,
+        cache: false,
+        success: function(data, textStatus) {
+            subject = data.subject;
+            content = data.content;
+        }
+    });
+
+    if (subject) {
+        alert("제목에 금지단어('"+subject+"')가 포함되어있습니다");
+        f.wr_subject.focus();
+        return false;
+    }
+
+    if (content) {
+        alert("내용에 금지단어('"+content+"')가 포함되어있습니다");
+        if (typeof(ed_wr_content) != "undefined")
+            ed_wr_content.returnFalse();
+        else
+            f.wr_content.focus();
+        return false;
+    }
+
+    if (document.getElementById("char_count")) {
+        if (char_min > 0 || char_max > 0) {
+            var cnt = parseInt(check_byte("wr_content", "char_count"));
+            if (char_min > 0 && char_min > cnt) {
+                alert("내용은 "+char_min+"글자 이상 쓰셔야 합니다.");
+                return false;
+            }
+            else if (char_max > 0 && char_max < cnt) {
+                alert("내용은 "+char_max+"글자 이하로 쓰셔야 합니다.");
+                return false;
+            }
+        }
+    }
+
+    <?php if ($is_guest) { echo chk_captcha_js(); } ?>
+
+    document.getElementById("btn_submit").disabled = "disabled";
+
+    return true;
+}
+</script>
